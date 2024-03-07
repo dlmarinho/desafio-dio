2f0d0b5de5f73e9a0c2074c68c45ec208bb1659d
dlmarinho <marinhodaniel71@gmail.com>
2024-03-07 19:07:59 -0300

    Projeto dio finalisado com else-if

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log("Ola mundo")
-console.log("Isso é um teste")
+let nomeHoreoi = "Daniel"
+let experienciaHeroi = 7001
+let nivelHeroi
+
+if (experienciaHeroi <= 1000) {
+    nivel = "Ferro"
+} else if ((experienciaHeroi >= 1001) && (experienciaHeroi <= 2000) ) {
+    nivel = "Bronze"
+}else if ((experienciaHeroi >= 2001) && ( experienciaHeroi <= 5000) ) {
+    nivel = "Prata" 
+}else if ((experienciaHeroi >= 5001) && ( experienciaHeroi <= 7000) ) {
+    nivel = "Ouro" 
+}else if ((experienciaHeroi >= 7001) && ( experienciaHeroi <= 8000) ) {
+    nivel = "Platina"
+}else if ((experienciaHeroi >= 8001) && ( experienciaHeroi <= 9000) ) {
+    nivel = "Ascendente" 
+}else if ((experienciaHeroi >= 9001) && ( experienciaHeroi <= 10000) ) {
+    nivel = "Imortal" 
+}else {
+    nivel = "Radiante"
+}
+
+console.log("O heroi de nome " +  nomeHoreoi + " está no nível de " + nivel)
